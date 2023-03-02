a728ff92a509ff11ccfdfaa2b2879d5bdbf5dede
Inna_V <zfp.je23@gmail.com>
2023-03-02 23:08:16 +0300

    add second-slider

--- slider.js
+++ slider.js
@@ -9,31 +9,45 @@ let images = [{
     title: 'Rostov-on-Don Patriotic'
 }];
 
+let imagesSecond = [{
+    url: "img/galery/image.png"
+},{
+    url: "img/galery/image2.png"
+},{
+    url: "img/galery/imag3.png"
+}];
+
 function initSlider() {
 
     let sliderImg = document.querySelector(".slider");
-    let sliderArrows = document.querySelector(".div-arrows");
     let sliderDots = document.querySelector(".slider_dots");
-    let sliderTitle = document.querySelector(".admiral")
+    let sliderTitle = document.querySelector(".admiral");
+    let sliderSecond = document.querySelector(".galery-slider");
 
-    let sliderArrowsMobile = document.querySelector(".arrows_mobile")
-
-    initImages();
+    initImages(images, sliderImg);
     initArrows();
     initDots();
     initTitle();
+    
+    initImages(imagesSecond, sliderSecond);
+    initSliderSecond();
+    addClassImage();
 
-    initArrowsMobile();
-
-    function initImages() {
-        images.forEach ((image, index) => {
-            let imageDiv = `<div class="image n${index} ${index === 0? "active" : ""}" style="background-image:url(${images[index].url});" data-index="${index}"></div>`;
-            sliderImg.innerHTML += imageDiv;
+    function initImages(img, wrapper) {
+        img.forEach ((image, index) => {
+            let imageDiv = `<div class="image n${index} ${index === 0? "active" : ""}" style="background-image:url(${img[index].url});" data-index="${index}"></div>`;
+            wrapper.innerHTML += imageDiv;
         });
     }
 
+    function addClassImage() {
+        sliderSecond.querySelectorAll(".image").forEach(index => {
+            index.classList.add("image-mobile");
+        })
+    }
+
     function initArrows() {
-        sliderArrows.querySelectorAll (".slider_arrows").forEach (arrow => {
+        document.querySelectorAll(".slider_arrows").forEach (arrow => {
             arrow.addEventListener ('click', function() {
                 let curNumber = +sliderImg.querySelector(".active").dataset.index;
         let nextNumber;
@@ -71,23 +85,6 @@ function initSlider() {
         })
     }
 
-    function initArrowsMobile() {
-        sliderArrowsMobile.querySelectorAll (".arrow_mobile-item").forEach (arrow => {
-            arrow.addEventListener ('click', function() {
-                let curNum = +sliderImg.querySelector(".active").dataset.index;
-                let nextNum;
-                if (arrow.classList.contains("rigth")) {
-                    nextNum = curNum === images.length - 1? 0 : curNum + 1;
-                } else {
-                    nextNum = curNum === 0? images.length - 1 : curNum - 1;
-                }
-                moveSlider(nextNum);
-            })
-        })
-    }
-
-
-
     function moveSlider(num) {
         sliderImg.querySelector(".active").classList.remove("active");
         sliderImg.querySelector(".n" + num).classList.add("active");
@@ -99,7 +96,26 @@ function initSlider() {
         sliderTitle.querySelector(".n" + num).classList.add("active");
     }
 
+    function initSliderSecond() {
+        document.querySelectorAll(".slider_arrows-second").forEach (arrow => {
+            arrow.addEventListener ('click', function() {
+                let curNumber = +sliderSecond.querySelector(".active").dataset.index;
+        let nextNumber;
+        if (arrow.classList.contains("left-arr")) {
+          nextNumber = curNumber === 0? images.length - 1 : curNumber - 1;
+        } else {
+          nextNumber = curNumber === images.length - 1? 0 : curNumber + 1;
+        }
+        moveSliderSecond(nextNumber);
+            })
+        })
 
+        function moveSliderSecond(number) {
+            sliderSecond.querySelector(".active").classList.remove("active");
+            sliderSecond.querySelector(".n" + number).classList.add("active");
+        }
+    }
+    
 }
 
 document.addEventListener("DOMContentLoaded", initSlider);
